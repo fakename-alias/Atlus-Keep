0fdf456714fd68ceb08f0c2e884f7185900cfaac
LuckyLsoer <skooaskl@gmail.com>
2025-09-15 05:46:51 +1200

    Changed how turn ordering works
    - The system now calculates ahead a few turns in order to display how many turns away each entity is.
    - Grouped all the outputs into one area (there are like 2 print statements) to make integrating into existing systems easier.
    - Added an action queue to make dealing with adding or removing entities mid-combat possible.
    - Each entity now displays how many turns until their next turn.
    - Added some temporary commands for testing purposes: press SPACE to end turn, press Q to add entities, press E to remove a random entity

--- turn order/turn_order.gd
+++ turn order/turn_order.gd
@@ -1,29 +1,61 @@
 extends Node
 
 var entity_list = []
+var action_queue = []
+var simulated_turn = 0
 var current_turn = 0
+
+var e_no # Temporary variable for testing purposes
 
 @export var entity_path: Node # Path to where the entities are stored, change as necessary
 
 func _ready() -> void:
+	e_no = get_children().size() + 1 # Temporary variable for testing purposes
 	entity_list = get_entities()
+	# Uncomment these if you want to see each entity's Readiness Gain stat
 	#for e in entity_list:
 		#print (e.name + " is Playable: " + str(e.is_playable) + ", Readiness Gain: " + str(e.readiness_gain))
+	current_turn = 1
 
-func _physics_process(delta: float) -> void:
-	# Call entity_list = get_entities() in here when entities are removed or added (killed/summoned?)
-	# Actual turn ordering
-	if (entity_list.all(func(e): return e.turn_done)):
-		var ready_queue = decide_turn_order(prepare(entity_list))
-		for current_entity in ready_queue:
-			current_turn += 1
-			print("Current turn: " + str(current_turn))
-			current_entity.get_turn()
+func _process(delta: float) -> void:
+	# Calculate the turn order if not during any entity's turn
+	if (entity_list.all(func(entity): return entity.turn_done)):
+		# Remove from the action queue any removed entity
+		var removal_list = []
+		for i in action_queue.size():
+			if (entity_list.find(action_queue[i]) == -1):
+				removal_list.append(i)
+		while (!removal_list.is_empty()):
+			action_queue.remove_at(removal_list.pop_back())
+		# Caluclate ahead until each entity is in the action queue at least once
+		if (entity_list.any(func(entity): return (action_queue.find(entity) == -1))):
+			#print(action_queue)
+			# Create and sort the ready queue
+			for entity in decide_turn_order(prepare(entity_list)):
+				# Add the entity to the action queue
+				action_queue.append(entity)
+				entity.readiness = 0
+		# After each entity's next turn is known, display how many turns away is each entity's turn.
+		# Also call get_turn() for the current active entity.
+		else:
+			# Execute the action queue
+			if (!action_queue.is_empty()):
+				# Hook these print statements to the UI
+				print("Current turn: " + str(current_turn))
+				for entity in entity_list:
+					print(entity.name + " is " + str(action_queue.find(entity)) + " turn(s) away.")
+				action_queue.pop_front().get_turn() # <-- Where the current entity's turn is called
+				current_turn += 1
+				print("----------") # Just for readability on the debugger
+			else:
+				# If somehow all entities disappeared, both playable and non-playable, put something here.
+				print("Everyone died")
 
-# Get list of entities in the scene
-# Change as necessary
+# Get all the entities
 func get_entities() -> Array:
 	var e_list = []
+	# For now it just gets all nodes that are children of whatever node is at the end of
+	# entity_path that are also named "entity" + a number. Change as necessary
 	for node in entity_path.get_children():
 		if (node.name.match("entity*")):
 			e_list.append(node)
@@ -80,3 +112,17 @@ func decide_turn_order(readied_entities: Array) -> Array:
 	# Append into sorted array
 	sorted_readied_entities.append(fastest_e)
 	return sorted_readied_entities
+
+# Temporary commands for testing purposes
+func _input(event: InputEvent) -> void:
+	# Press Q to add a new entity
+	if ((event.as_text() == "Q") and event.is_released()):
+		var node = preload("res://turn order/entity_placeholder.tscn").instantiate()
+		node.set_name("entity" + str(e_no))
+		add_child(node)
+		e_no += 1
+		entity_list = get_entities()
+	# Press E to remove a random entity
+	elif ((event.as_text() == "E") and event.is_released()):
+		remove_child(entity_list[randi() % entity_list.size()])
+		entity_list = get_entities()
